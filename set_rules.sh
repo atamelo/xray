67c26a8bd24d69282b64768f5bf31a2d31a9e510
atamelo <andrey.tamelo@gmail.com>
2024-10-27 12:37:46 -0700

    Update set_rules.sh

--- set_rules.sh
+++ set_rules.sh
@@ -3,6 +3,8 @@
 #        option lookup 'vpn'
 #        option mark '0x1'
 ip rule add fwmark 1 table 100
+
+# /etc/rc.local
 ip route add local 0.0.0.0/0 dev lo table 100
 
 iptables-restore iptables.rulesv4
