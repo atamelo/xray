bce914e4c36d0f8fc39fe701d5511e29192016a9
atamelo <andrey.tamelo@gmail.com>
2024-10-27 12:32:29 -0700

    Update set_rules.sh

--- set_rules.sh
+++ set_rules.sh
@@ -1,3 +1,7 @@
+# config rule
+#        option priority '100'
+#        option lookup 'vpn'
+#        option mark '0x1'
 ip rule add fwmark 1 table 100
 ip route add local 0.0.0.0/0 dev lo table 100
 
